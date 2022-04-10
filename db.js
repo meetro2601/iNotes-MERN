6434e8596631c33e90148b4aa15255edb069c0af
Meet Rojivadiya <meetbr26@gmail.com>
2022-04-10 18:18:13 +0530

    Connected to MongoDB Atlas

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 const connectToMongo = ()=>{
-        mongoose.connect(process.env.MONGO_COMPASS_URI)
+        mongoose.connect(process.env.MONGO_ATLAS_URI)
         .then(()=>console.log('Connected'))
         .catch((error)=>console.log(error))
 }
