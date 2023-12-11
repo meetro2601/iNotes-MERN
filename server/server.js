a0a30e2058d346eb76d644903972b84dbc14f959
Meet Rojivadiya <meetbr26@gmail.com>
2023-12-11 19:11:08 +0530

    Credentials fixing

--- server/server.js
+++ server/server.js
@@ -16,22 +16,25 @@ connectToMongo()
 //initializing app 
 const app = express()
 app.use(json())
-app.use(cors({ 
-    origin: ['https://genuine-mousse-507905.netlify.app', 'http://localhost:3000'] }))
+app.use(cors({
+    origin: ['https://genuine-mousse-507905.netlify.app', 'http://localhost:3000'],
+    credentials: true,
+}
+))
 app.use(cookieParser())
 app.use('/auth', auth)
-app.use('/auth',googleAuth)
-app.use('/notes',notes)
-app.use('/password',password)
+app.use('/auth', googleAuth)
+app.use('/notes', notes)
+app.use('/password', password)
 
 
 // const __dirname = url.fileURLToPath(new URL('.', import.meta.url)); OR
 const __dirname = resolve()
 
-if(process.env.NODE_ENV == 'production'){
-    app.use(express.static(join(__dirname,'app','build')))
-    app.get('*',(req,res)=>{
-        res.sendFile(join(__dirname,'app','build','index.html'),(err)=>{
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static(join(__dirname, 'app', 'build')))
+    app.get('*', (req, res) => {
+        res.sendFile(join(__dirname, 'app', 'build', 'index.html'), (err) => {
             res.status(500).send(err)
         })
     })
@@ -39,6 +42,6 @@ if(process.env.NODE_ENV == 'production'){
 
 const port = process.env.PORT || 2601
 //app is listening on port 2601
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server is running on port ${port}`)
 })
